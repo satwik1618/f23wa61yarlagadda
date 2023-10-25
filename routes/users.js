dc54afafeac4df2e92317572c21dc3dcbffd5415
Satwik Yarlagadda <yarlagaddasatwikchowdary@gmail.com>
2023-10-25 16:52:55 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  more=more+1;
+  sum=sum+more;
   res.send('respond with a resource');
 });
 
 module.exports = router;
+var sum =0;
+var more=0;
